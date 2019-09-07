bcf892414982f401e1cade28a614ff09e1459927
mmthant03 <mmthant@wpi.edu>
2019-09-07 16:10:18 -0400

    added k-fold

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 from keras.utils import to_categorical
 from keras import optimizers
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 from matplotlib import pyplot as plt
-
 
 # loading data from images.npy and labels.npy
 imgData = np.load('images.npy')
@@ -16,30 +16,42 @@ imgData = imgData.reshape((-1, 784))
 
 # split into 20% test set, 20% validation set, and 60% training set
 X_train, X_test, Y_train, Y_test = train_test_split(imgData, labelData, test_size=0.2, random_state=1)
-#X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1)
+
 
 # build the model
+# @param hiddenLayer int, number of hidden layers
+# @param dropOut boolean, True for dropOut
+# @param dropSize int, number of size to drop out on the first layer
+# example from task2:
 # 1 input layer with 784 inputs
 # 10 hidden layers with 50 nodes each
 # 1 output layer with 1 node
-model = Sequential([
-    Dense(50, activation='relu', input_shape=(784,)), # hidden layer 1 with input 
-    Dense(50, activation='relu'), # hidden layer 2
-    Dense(50, activation='relu'), # hidden layer 3
-    Dense(50, activation='relu'), # hidden layer 4
-    Dense(50, activation='relu'), # hidden layer 5
-    Dense(50, activation='relu'), # hidden layer 6
-    Dense(50, activation='relu'), # hidden layer 7
-    Dense(50, activation='relu'), # hidden layer 8
-    Dense(50, activation='relu'), # hidden layer 9
-    Dense(50, activation='relu'), # hidden layer 10
-    Dense(10, activation='softmax'), # output layer <== still not sure about the softmax function
-])
+# New model with dropout 20%
+def create_model(hiddenLayer, dropOut, dropSize):
+    # instantiate a new model
+    model = Sequential()
+    if(dropOut): # if dropOut, add a drop-out to input layer
+        model.add(Dropout(dropSize, input_shape=(784,), seed=1))
+    else: # else, just add an input layer
+        model.add(Dense(50, activation='relu', input_shape=(784,)))
+        hiddenLayer = hiddenLayer - 1
+    
+    # depending on the numbers of given hidden layers, this loop will create hidden layers
+    while(hiddenLayer > 0):
+        model.add(Dense(50, activation='relu'))
+        hiddenLayer = hiddenLayer - 1
 
-sgd = optimizers.SGD(lr=0.001)
+    # add a output layer
+    model.add(Dense(10, activation='softmax'))
+    
+    #return this model
+    return model
 
 # Stochastic gradient descent with 0.001 learning rate
+sgd = optimizers.SGD(lr=0.001)
+
 # Categorical cross-entropy loss function
+model = create_model(10, True, 0.2)
 model.compile(
     optimizer=sgd,
     loss='categorical_crossentropy',
@@ -62,10 +74,13 @@ history = model.fit(
 #     to_categorical(Y_test, num_classes=10),
 #     batch_size=512
 # )
-model.predict(
+prediction = model.predict(
     X_test,
     batch_size=512
 )
+
+confusion = confusion_matrix(Y_test, np.argmax(prediction, axis=1))
+print(confusion)
 
 # plot the accuracy of training set and validation set over epochs
 plt.plot(history.history['acc'])
